a5627ae7ec7fd9f319581001c3b5071fe5308ead
19036308 <19036308@myrp.edu.sg>
2021-05-27 12:04:51 +0800

    Done Frag1 without checking

---
+++ app/src/main/java/sg/edu/rp/c346/id19045784/knowyourfacts/Frag1.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.id19045784.knowyourfacts;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class Frag1 extends Fragment {
+
+    Button btnChangeColor;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        btnChangeColor = getView().findViewById(R.id.btnChangeColor);
+
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getView().setBackgroundColor(Color.parseColor("#BFDBFF"));
+            }
+        });
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_frag1, container, false);
+    }
+}
