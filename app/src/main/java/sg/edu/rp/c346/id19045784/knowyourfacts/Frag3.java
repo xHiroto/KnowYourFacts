99f7677f914026d3ca011da5eacfb752b8827705
Javier <19045784@myrp.edu.sg>
2021-05-27 12:26:18 +0800

    done Frag3

---
+++ app/src/main/java/sg/edu/rp/c346/id19045784/knowyourfacts/Frag3.java
@@ -0,0 +1,97 @@
+package sg.edu.rp.c346.id19045784.knowyourfacts;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Frag3#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Frag3 extends Fragment {
+    LinearLayout ll;
+    TextView tvFacts;
+    Button btnChangeColor;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Frag3() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Frag3.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Frag3 newInstance(String param1, String param2) {
+        Frag3 fragment = new Frag3();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_frag3, container, false);
+        ll = view.findViewById(R.id.Frag3Layout);
+        tvFacts = view.findViewById(R.id.Frag3tv);
+        btnChangeColor = view.findViewById(R.id.Frag3btn);
+
+        // create object of Random class
+        Random obj = new Random();
+        int rand_num = obj.nextInt(0xffffff + 1);
+
+        // format it as hexadecimal string and print
+        String colorCode = String.format("#%06x", rand_num);
+
+
+
+        tvFacts.setText("Coca-Cola would be green if colouring weren’t added to it. \n\n Earth is the only planet not named after a god \n\n  It is impossible to sneeze with your eyes open");
+
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ll.setBackgroundColor(Color.parseColor(colorCode));
+            }
+        });
+        return view;
+    }
+}
