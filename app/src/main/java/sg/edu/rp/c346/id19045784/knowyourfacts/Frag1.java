23028b05badc120704a12b814db171093bb9dd25
19036308 <19036308@myrp.edu.sg>
2021-05-27 13:02:59 +0800

    edited Frag1.java the getView() to view

--- app/src/main/java/sg/edu/rp/c346/id19045784/knowyourfacts/Frag1.java
+++ app/src/main/java/sg/edu/rp/c346/id19045784/knowyourfacts/Frag1.java
@@ -24,7 +24,7 @@ public class Frag1 extends Fragment {
         btnChangeColor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getView().setBackgroundColor(Color.parseColor("#BFDBFF"));
+                view.setBackgroundColor(Color.parseColor("#BFDBFF"));
             }
         });
         // Inflate the layout for this fragment
