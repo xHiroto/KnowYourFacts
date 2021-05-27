308294f098403fd0e8388818002c4f2c9c489630
19036308 <19036308@myrp.edu.sg>
2021-05-27 14:55:35 +0800

    edited the text

--- app/src/main/java/sg/edu/rp/c346/id19045784/knowyourfacts/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19045784/knowyourfacts/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         btnReadLtr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                finish();
 
             }
         });
